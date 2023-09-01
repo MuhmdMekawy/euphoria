9ab6f2363c325eb65bb4caf20be6917d4b4aa050
mkkawy <mekawy849@gmail.com>
2023-09-01 16:42:23 +0300

    test6

--- src/components/ProductSlider.vue
+++ src/components/ProductSlider.vue
@@ -1,23 +1,3 @@
-<template>
-  <div class="container">
-    <Carousel
-      :slides="slides"
-      :itemsToShow="4"
-      :breakpoints="breakpoints"
-      :wrap-around="true"
-      :autoplay="2000"
-      :loop="true"
-      class="carousel">
-
-      <slide v-for="slide in slides" :key="slide.imageUrl" class="slider">
-        <img :src="slide.imageUrl"  loading="lazy"/>
-        <p class="caption">{{ slide.caption }}</p>
-      </slide>
-
-    </Carousel>
-  </div>
-</template>
-
 <script>
 // If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
 import 'vue3-carousel/dist/carousel.css'
@@ -34,14 +14,14 @@ export default {
   data() {
     return {
       slides: [
-        { imageUrl: '../assets/images/Img1.png', caption: 'Knitted Joggers' },
-        { imageUrl: '../assets/images/2.png', caption: 'Full Sleeve' },
-        { imageUrl: '../assets/images/3.png', caption: 'Active T-Shirts' },
-        { imageUrl: '../assets/images/4.png', caption: 'Urban Shirts' },
-        { imageUrl: '../assets/images/1.png', caption: 'Knitted Joggers' },
-        { imageUrl: '../assets/images/2.png', caption: 'Full Sleeve' },
-        { imageUrl: '../assets/images/3.png', caption: 'Active T-Shirts' },
-        { imageUrl: '../assets/images/4.png', caption: 'Urban Shirts' },
+        { imageUrl: '/src/assets/images/Img1.png', caption: 'Knitted Joggers' },
+        { imageUrl: '/src/assets/images/2.png', caption: 'Full Sleeve' },
+        { imageUrl: '/src/assets/images/3.png', caption: 'Active T-Shirts' },
+        { imageUrl: '/src/assets/images/4.png', caption: 'Urban Shirts' },
+        { imageUrl: '/src/assets/images/1.png', caption: 'Knitted Joggers' },
+        { imageUrl: '/src/assets/images/2.png', caption: 'Full Sleeve' },
+        { imageUrl: '/src/assets/images/3.png', caption: 'Active T-Shirts' },
+        { imageUrl: '/src/assets/images/4.png', caption: 'Urban Shirts' },
       ],
       // breakpoints are mobile first
       // any settings not specified will fallback to the carousel settings
@@ -71,6 +51,28 @@ export default {
   }
 }
 </script>
+
+<template>
+  <div class="container">
+    <Carousel
+      :slides="slides"
+      :itemsToShow="4"
+      :breakpoints="breakpoints"
+      :wrap-around="true"
+      :autoplay="2000"
+      :loop="true"
+      class="carousel">
+
+      <slide v-for="slide in slides" :key="slide.imageUrl" class="slider">
+        <img :src="slide.imageUrl"  loading="lazy"/>
+        <p class="caption">{{ slide.caption }}</p>
+      </slide>
+
+    </Carousel>
+  </div>
+</template>
+
+
 <style lang="scss" scoped> 
 .carousel{
   .slider{
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === 'production' ? '/https://euphoria-one.vercel.app/' : '/',
-
 })
